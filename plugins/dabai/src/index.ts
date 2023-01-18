9ae0cf2cbbc5e328db549a5b2c8c1688051941bc
Maid <zhp92@outlook.com>
2023-01-18 17:20:07 +0800

    update

--- plugins/dabai/src/index.ts
+++ plugins/dabai/src/index.ts
@@ -6,15 +6,8 @@ export const name = 'dabai'
 
 export function apply (ctx: Context) {
   ctx.command('dabai <message>').action(async (_, message) => {
-    // const urls = await registAndGetSubUrls()
-    // return Promise.resolve(urls.join('\n'))
-    return Promise.resolve('xxx')
-  })
-    // 如果收到“天王盖地虎”，就回应“宝塔镇河妖”
-  ctx.on('message', (session) => {
-    if (session.content === '天王盖地虎') {
-      session.send('宝塔镇河妖')
-    }
+    const urls = await registAndGetSubUrls()
+    return Promise.resolve(urls.join('\n'))
   })
 }
 async function registAndGetSubUrls (): Promise<String[]> {
@@ -26,13 +19,13 @@ async function registAndGetSubUrls (): Promise<String[]> {
     const email = getRandomEmail()
     try {
       await register(email, name, pad, '', '')
-      console.log('注册成功');
+      console.log('注册成功')
       const cookie = await login(email, pad, '')
-      console.log('登录成功');
+      console.log('登录成功')
       await buy(cookie)
-      console.log('激活成功');
+      console.log('激活成功')
       const v2raySubUrl = await getV2rayUrl(cookie)
-      console.log('获取订阅连接成功');
+      console.log('获取订阅连接成功')
       allV2raySubUrls.push(v2raySubUrl)
     } catch (error) {
       console.error(`登录账号${email}时, 发生错误:${error}`)
